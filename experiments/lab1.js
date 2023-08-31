581f4c0b0a724c05656da3894e32bf7f950089da
PhongTran02 <93583320+PhongTran02@users.noreply.github.com>
2023-08-31 11:52:54 +0200

    2nd tryout

--- experiments/lab1.js
+++ experiments/lab1.js
@@ -12,24 +12,61 @@ background(255, 255, 255);
 // const roundRandomNumber = Math.floor(randomNumber);
 // console.log(roundRandomNumber);
 
-let gutter = 50;
-let amount = 10; 
+// let gutter = 50;
+// let amount = 8; 
+// function draw () {  
+//     for (let x = 0; x < amount; x++){
+//         for (let y = 0; y < amount; y++) {
+//             // ellipse (100 + x * 50, 100 + y* 50, 40);
+//             push ();
+//             if (Math.random() > 0.5) {
+//                 push ();
+//                 fill (120, 36, 189);
+//                 translate (x, y);
+//                 // rectMode(CENTER);
+//                 rect (100 + x * gutter, 100 + y* gutter, 20);  
+//                 pop ();
+//             }else {
+//                 ellipse (100 + x * gutter, 100 + y* gutter, 20);
+//             }
+//             pop();
+//         }
+//     }
+//     noLoop ();    
+// }
+let gutter = 100;
+let gridWidth = 400; 
+let gridHeight = 400; 
+let x = (width - gridWidth )/2;
+let y = (height - gridHeight )/2;
+
+let rectSize = 50;
+let rectAmount = 5;
+
+console.log(x); 
+console.log(y); 
 function draw () {  
-    for (let x = 0; x < amount; x++){
-        for (let y = 0; y < amount; y++) {
-            // ellipse (100 + x * 50, 100 + y* 50, 40);
+push(); 
+   translate (x, y);
+   for ( let x = 0; x < rectAmount; x++) {
+    for (let y = 0; y < rectAmount; y++) {
+        push();
+        if (Math.random() > 0.8) {
+        translate (x * gutter, y * gutter);
+        rectMode (CENTER);
+        rect (0,0, rectSize);
+        pop();
+        } else if (Math.random () < 0.4) {
+            strokeWeight (10);  
+        }else {
             push ();
-            if (Math.random() > 0.5) {
-                push ();
-                fill (120, 36, 189);
-                translate (x, y);
-                rect (100 + x * gutter, 100 + y* gutter, 20);  
-                pop ();
-            }else {
-                ellipse (100 + x * gutter, 100 + y* gutter, 30);
-            }
-            pop();
+            fill (234, 57, 120);
+            translate (x * gutter, y * gutter);
+            ellipse (0, 0, 20);  
+            pop ();
         }
     }
-    noLoop ();    
+   }
+   noLoop ();
+pop();
 }
